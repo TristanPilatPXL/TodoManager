833ea85dea55c5aff783299e129d03d25d044fe3
TristanPilatPXL <Tristan.Pilat@STUDENT.PXL.BE>
2026-02-23 15:38:07 +0100

    TODO-01: TodoItem klaar

--- TodoManager/TodoManager.Domain/Models/TodoItem.cs
+++ TodoManager/TodoManager.Domain/Models/TodoItem.cs
@@ -8,5 +8,43 @@ namespace TodoManager.Domain.Models
 {
     internal class TodoItem
     {
+        public int Id { get; set; }
+        public string Title { get; }
+        public string? Description { get; }
+        public DateTime DueDate { get; }
+        public bool IsCompleted { get; private set; }
+        public DateTime? CompletedAt { get; private set; }
+
+        public TodoItem(string title, string? description, DateTime dueDate)
+        {
+
+            //titel moet
+            if (string.IsNullOrWhiteSpace(title))
+            {
+                throw new ArgumentNullException("title cannot be null, empty, or whitespace");
+            }
+
+
+            //dit mag null zijn
+            description = description?.Trim();
+
+            IsCompleted = false;
+            CompletedAt = null;
+        }
+
+        public void MarkAsCompleted()
+        {
+
+            IsCompleted = true;
+            CompletedAt = DateTime.Now;
+         
+        }
+
+        public override string ToString()
+        {
+            string status = IsCompleted ? "[Done]" : "[Open]";
+            return $"{status} {Title} (due {DueDate:dd/MM/yyyy})";
+        }
     }
+
 }
